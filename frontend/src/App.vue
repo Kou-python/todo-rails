5131bf0312a2cebcba08181f66f965b0e3a8a921
Kou-python <25930127+Kou-python@users.noreply.github.com>
2025-06-09 10:12:57 +0900

    [wip]frontend作成段階

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- html -->
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// js
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/todos')
+      .then(response => {
+        this.todos = response.data
+      })
+  }
+}
+</script>
