fe47e8c51c0de3dc9f9df41d65296ccea12a6b1e
Kou-python <25930127+Kou-python@users.noreply.github.com>
2025-06-16 18:47:43 +0900

    コードスタイルの統一: 不要な空白を削除し、CORS設定の整形を行いました

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -2,10 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Vueの起動ポートに合わせる
+    origins "http://localhost:5173" # Vueの起動ポートに合わせる
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
